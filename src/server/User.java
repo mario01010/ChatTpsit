263c044fd7d44a6f0c7bc4dc2dbbbb21c42cf194
mario01010 <luchi2007radiolina@gmail.com>
2025-11-11 21:02:25 +0100

    chat lavoro tpsit

---
+++ src/server/User.java
@@ -0,0 +1,24 @@
+public class User {
+    private String userID;
+    private String username;
+    private String password;
+    private Chat chat;
+
+    public User(String userID, String username, String email) {
+        this.userID = userID;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
